759b4b9f7c2aa3332589c2a254bece66ce0d4e14
techoid <techoid.dev@gmail.com>
2022-10-18 21:18:25 +0500

    Update VMeals

---
+++ pages/components/detailscomponent2.js
@@ -0,0 +1,14 @@
+function detailscomponent() {
+    return <>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 top-container">
+                    <h1 class="top-heading">Our Partners</h1>
+                    <h1 class="top-sub-heading"><span class="house-icon"><i class="fa-solid fa-house"></i></span><span class="angle-arrows-icon"><i class="fa-solid fa-angle-right"></i></span><span class="sub-heading">Our Partners</span></h1>
+                </div>
+            </div>
+        </div>
+    </>
+};
+
+export default detailscomponent;
---
+++ pages/components/iconcomponents.js
@@ -0,0 +1,15 @@
+function iconcomponents() {
+    return <>
+        <div class="row section-row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 section-row-col">
+                <center>
+                    <a class="icon-container-padding-rigth" href="#"><i class="fa-brands fa-facebook-f icon-container-image"></i></a>
+                    <a class="icon-container-padding-rigth" href="#"><i class="fa-brands fa-instagram icon-container-image"></i></a>
+                    <a href="#"><i class="fa-brands fa-whatsapp icon-container-image"></i></a>
+                </center>
+            </div>
+        </div>
+      </>
+  };
+  
+  export default iconcomponents;
